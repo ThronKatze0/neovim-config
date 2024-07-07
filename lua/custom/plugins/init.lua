4653127a782f3b93be7eac8af5ab130dbae4e538
mageOfstructs <jason.puschnig@gmx.at>
2024-07-07 18:45:18 +0200

    enable plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,29 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup()
+    end,
+  },
+  {
+    'uga-rosa/ccc.nvim',
+    setup = function()
+      require('ccc').setup {
+        highlighter = {
+          auto_enable = true,
+          lsp = true,
+        },
+      }
+    end,
+  },
+  'sindrets/diffview.nvim',
+  {
+    'zeioth/garbage-day.nvim',
+    dependencies = 'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
+    opts = {
+      -- your options here
+    },
+  },
+  require 'lazygit',
+}
