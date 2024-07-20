b6716c8e1a10b19d51fc1328283f2e71779cf5f2
mageOfstructs <jason.puschnig@gmx.at>
2024-07-20 19:48:22 +0200

    add coq
    Note: Need to run COQdeps, COQnow to install

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -43,6 +43,7 @@ require('lazy').setup({
   require 'plugins/tokyonight',
   require 'plugins/treesitter',
   require 'plugins/which-key',
+  require 'plugins/coq',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
