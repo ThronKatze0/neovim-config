31f512bb6cd2ecd7e591ce497ac5246e9472e539
mageOfstructs <jason.puschnig@gmx.at>
2024-07-27 20:31:31 +0200

    remove Lazy keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,7 +34,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Open Lazy' })
+-- vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Open Lazy' })
 
 vim.keymap.set('i', '<Tab>', '<CMD>lua require("intellitab").indent()<CR>')
 
